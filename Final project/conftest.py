1212f3b04d93c01b4524ff59a191056ddeb58d5e
e.romaschuk <shumak@gmail.com>
2022-08-23 09:43:13 +0300

    Added files: __init__, conftest, requirements and test

---
+++ Final project/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options as FFO
+
+#@pytest.fixture(scope="function")
+#def browser():
+#    print("\nstart browser for test..")
+#    options = webdriver.ChromeOptions()
+#    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#   browser = webdriver.Chrome(options=options)
+#   yield browser
+#  print("\nquit browser..")
+# browser.quit()
+ 
+
+
+
+#pytest -s -v --browser_name=<firefox or chrome> --language=<en/ru/es> file-name.py
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en-gb',
+                     help="Choose language: ru/en/es ..")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = FFO()
+        fp.set_preference('intl.accept_languages', user_language)
+        service = Service(r'C:\geckodriver\geckodriver.exe')
+        browser = webdriver.Firefox(service=service, options=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
